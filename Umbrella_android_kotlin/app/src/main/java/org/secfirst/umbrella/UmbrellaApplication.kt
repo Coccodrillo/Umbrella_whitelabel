191e1fc64297c11a0b79065cdf977cbc48d26baa
Douglas Mesquita <douglas.alipio@gmail.com>
2018-05-11 15:57:54 +0100

    detekt file changed.

--- Umbrella_android_kotlin/app/src/main/java/org/secfirst/umbrella/UmbrellaApplication.kt
+++ Umbrella_android_kotlin/app/src/main/java/org/secfirst/umbrella/UmbrellaApplication.kt
@@ -9,10 +9,18 @@ import com.raizlabs.android.dbflow.config.FlowManager
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
 import net.sqlcipher.database.SQLiteDatabase
+import org.eclipse.jgit.api.Git
 import org.secfirst.umbrella.data.local.AppDatabase
 import org.secfirst.umbrella.data.local.SQLCipherHelperImpl
 import org.secfirst.umbrella.di.component.DaggerAppComponent
+import java.io.File
+import java.util.*
 import javax.inject.Inject
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder
+
+
+
+
 
 
 class UmbrellaApplication : Application(), HasActivityInjector {
@@ -48,8 +56,28 @@ class UmbrellaApplication : Application(), HasActivityInjector {
                         }
                         .build())
                 .build()
+
         FlowManager.init(dbConfig)
         FlowLog.setMinimumLoggingLevel(FlowLog.Level.V)
+    }
+
+    private fun initTentRepository() {
+       val git =  Git.cloneRepository()
+                .setURI("https://github.com/klaidliadon/tent-sample.git")
+                .setDirectory(File(applicationContext.cacheDir.path + "/repo/"))
+                .setBranchesToClone(Arrays.asList("refs/heads/master"))
+                .setBranch("refs/heads/master")
+                .call()
+
+        val repositoryBuilder = FileRepositoryBuilder()
+        val repository = repositoryBuilder.setGitDir(File("/path/to/repo/.git"))
+                .readEnvironment() // scan environment GIT_* variables
+                .findGitDir() // scan up the file system tree
+                .setMustExist(true)
+                .build()
+        Git.open(File("/path/to/repo/.git"))
+                .checkout()
+        val te = git.getRepository()
     }
 
     override fun onTerminate() {
