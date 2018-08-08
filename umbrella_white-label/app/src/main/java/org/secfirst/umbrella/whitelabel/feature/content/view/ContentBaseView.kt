eca03c81726375c24cd23c31a0315ec63d00497a
Douglas Mesquita <douglas.alipio@gmail.com>
2018-08-08 20:30:43 +0100

    Removed standar-code

--- umbrella_white-label/app/src/main/java/org/secfirst/umbrella/whitelabel/feature/content/view/ContentBaseView.kt
+++
@@ -1,9 +0,0 @@
-package org.secfirst.umbrella.whitelabel.feature.content.view
-
-import org.secfirst.umbrella.whitelabel.feature.base.view.BaseView
-
-interface ContentBaseView : BaseView {
-
-    fun finishDownloadedData()
-    fun onErrorContentNotFound()
-}
