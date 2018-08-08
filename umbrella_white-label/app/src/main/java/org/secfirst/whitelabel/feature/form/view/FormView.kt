182a9758673e05e1d4ea3c9a6d7c6a5c02e0b901
Douglas Mesquita <douglas.alipio@gmail.com>
2018-08-08 20:15:13 +0100

    renamed package

--- umbrella_white-label/app/src/main/java/org/secfirst/whitelabel/feature/form/view/FormView.kt
+++
@@ -1,9 +0,0 @@
-package org.secfirst.whitelabel.feature.form.view
-
-import org.secfirst.whitelabel.data.Form
-import org.secfirst.whitelabel.feature.base.view.BaseView
-
-interface FormView : BaseView {
-    fun showModelForms(modelForms: List<Form>) {}
-    fun showActiveForms(activeForms: List<Form>) {}
-}
